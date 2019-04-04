fb730fe8d8634de49614d3d058cfe2fec7d66e1b
Mia Nordentoft <mia@miestasmia.com>
2019-04-04 16:42:13 +0200

    Full user auth 2FA support

--- src/user-client.js
+++ src/user-client.js
@@ -26,13 +26,14 @@ class UserClient {
 	 * @return {Object}          The response from `GET /auth`
 	 */
 	async logIn (login, password) {
-		await this.client.req({
+		await this.req({
 			method: 'PUT',
 			path: '/auth',
 			body: {
 				login: login,
 				password: password
-			}
+			},
+			_allowLoggedOut: true
 		});
 
 		const resAuthCheck = await this.client.req({
@@ -50,11 +51,52 @@ class UserClient {
 	 * Logs out
 	 */
 	async logOut () {
-		await this.client.req({
+		await this.req({
 			method: 'DELETE',
-			path: '/auth'
+			path: '/auth',
+			_allowLoggedOut: true
 		});
 		this.loggedIn = false;
+	}
+
+	/**
+	 * Sets up totp and logs in using it. The TOTP client must be instructed to set the period to 30 seconds
+	 * @param  {Buffer} secret 20 random bytes used as the TOTP secret
+	 * @param  {string} totp   A 6 character TOTP code
+	 */
+	async totpSetup (secret, totp) {
+		await this.req({
+			method: 'POST',
+			path: '/auth/totp',
+			body: {
+				secret: secret,
+				totp: totp
+			}
+		});
+	}
+
+	/**
+	 * Logs in using TOTP
+	 * @param  {string} totp A 6 character TOTP code
+	 */
+	async totpLogIn (totp) {
+		await this.req({
+			method: 'POST',
+			path: '/auth/totp',
+			body: {
+				totp: totp
+			}
+		});
+	}
+
+	/**
+	 * Disables TOTP
+	 */
+	async totpRemove () {
+		await this.req({
+			method: 'DELETE',
+			path: '/auth/totp'
+		});
 	}
 
 	/**
@@ -62,7 +104,7 @@ class UserClient {
 	 * Makes a request to the AKSO API
 	 */
 	req (options) {
-		if (!this.loggedIn) { throw new Error('Call UserClient#logIn to log in first'); }
+		if (!this.loggedIn && !options._allowLoggedOut) { throw new Error('Call UserClient#logIn to log in first'); }
 
 		options.credentials = 'include';
 		return this.client.req(options);
