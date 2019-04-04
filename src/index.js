4d7979f89dd8406420bae1222435fe8922e26483
Mia Nordentoft <mia@miestasmia.com>
2019-04-04 14:55:34 +0200

    Removed missing export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import msgpack from 'msgpack-lite';
 
 export { default as UEACode } from './uea-code';
 export { default as AppClient } from './app-client';
-export { default as AKSOResponse } from './akso-response';
 
 const msgpackCodec = msgpack.createCodec({
 	int64: true
