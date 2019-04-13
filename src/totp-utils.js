6a6dbb214dcb5639e49fc9043b2f8690e4c648b0
Mia Nordentoft <mia@miestasmia.com>
2019-04-13 11:44:28 +0200

    Added documentation

--- src/totp-utils.js
+++ src/totp-utils.js
@@ -3,6 +3,11 @@ import { promisify } from 'util';
 import { base32 } from 'rfc4648';
 import QRCode from 'qrcode';
 
+/**
+ * Generates a TOTP key
+ * @param  {string} ueaCode The user's UEA code
+ * @return {Object} An object containing `Buffer secret`, `string otpURL` and `string qrCode` (data url)
+ */
 export async function generateTotp (ueaCode) {
 	const secret = await promisify(crypto.randomBytes)(20);
 	const secretBase32 = base32.stringify(secret);
