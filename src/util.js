4b6b57f1c07de02f11aca749174fb8d03c8b7132
Mia Nordentoft <mia@miestasmia.com>
2019-05-12 15:01:40 +0200

    Added util.transformSearch

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import XRegExp from 'xregexp';
+
 /**
  * Checks recursively whether an object contains a buffer
  * @param  {Object} obj
@@ -15,4 +17,30 @@ export function containsBuffer (obj) {
 			.reduce((a, b) => a || b, false);
 	}
 	return false;
+}
+
+const bannedSearchChars = new XRegExp('[^\\p{L}\\p{N}\\s*+\\-"]', 'g');
+const searchWordChars = new XRegExp('([\\p{L}\\p{N}]+)', 'g');
+const searchOperators = /[*+\-"]/;
+const searchWordsTooShort = new XRegExp('(^|[^\\p{L}\\p{N}])([\\p{L}\\p{N}]{1,2})([^\\p{L}\\p{N}]|$)', 'g');
+/**
+ * Transforms a search string into what the user probably wanted
+ * @param  {string} str
+ * @return {string}
+ */
+export function transformSearch (str) {
+	str = str.replace(bannedSearchChars, ' ');
+
+	const containsOperators = searchOperators.test(str);
+
+	if (containsOperators) {
+		str = str.replace(searchWordsTooShort, '$1$2*$3');
+	} else {
+		str = str
+			.match(searchWordChars)
+			.map(w => w + '*')
+			.join(' ');
+	}
+
+	return str;
 }
