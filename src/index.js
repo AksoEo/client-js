40462477d8a8db0f418dab8f8b1833ea55faac1c
Mia Nordentoft <mia@miestasmia.com>
2019-05-13 13:35:50 +0200

    export util

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import msgpack from 'msgpack-lite';
 
+import * as util from './util';
+export { util };
+
 export { default as Client } from './client';
 export { default as AppClient } from './app-client';
 export { default as UserClient } from './user-client';
