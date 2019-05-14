f3c2915822ef11233c30bdec28df2a91a3ccbaab
Mia Nordentoft <mia@miestasmia.com>
2019-05-14 11:21:27 +0200

    Fix broken wildcard usage in transformSearch

--- src/util.js
+++ src/util.js
@@ -22,7 +22,15 @@ export function containsBuffer (obj) {
 const bannedSearchChars = new XRegExp('[^\\p{L}\\p{N}\\s*+\\-"]', 'g');
 const searchWordChars = new XRegExp('([\\p{L}\\p{N}]+)', 'g');
 const searchOperators = /[*+\-"]/;
-const searchWordsTooShort = new XRegExp('(^|[^\\p{L}\\p{N}])([\\p{L}\\p{N}]{1,2})([^\\p{L}\\p{N}]|$)', 'g');
+const searchWordsTooShort = new XRegExp(
+	`
+	(^|[^\\p{L}\\p{N}]) # Ensure it's the beginning of a word
+
+	([\\p{L}\\p{N}]{1,2})(?!\\*) # Match any word shorter than three chars that doesn't have a wildcard at the end
+
+	(?=$|[^\\p{L}\\p{N}]) # Ensure it's the end of a word
+	`,
+	'gx');
 /**
  * Transforms a search string into what the user probably wanted
  * @param  {string} str
