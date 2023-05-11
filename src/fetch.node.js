2905f7f9e5050e5183c85bac1c6691759b611e60
cpsdqs <cpsdqs@cloudwithlightning.net>
2023-05-11 18:36:25 +0200

    fix outdated fetch-cookie

--- src/fetch.node.js
+++ src/fetch.node.js
@@ -5,15 +5,3 @@ export const Headers = global.Headers;
 export const FormData = global.FormData;
 export const Blob = global.Blob;
 export const IS_WEB = false;
-
-// fetch-cookie expects Headers to have a .raw() method that returns an object of all headers
-Object.defineProperty(Headers.prototype, 'raw', {
-	value () {
-		const values = {};
-		for (const key of this.keys()) {
-			values[key] = this.get(key);
-		}
-		return values;
-	},
-	enumerable: false,
-});
